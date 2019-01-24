ab63c33d9fc76e638f10bd4ad2b9dea920cb6dbd
DenisKrivenko <job.denis.krivenko@gmail.com>
2019-01-24 18:33:29 +0200

    new function

--- java.js
+++ java.js
@@ -15,11 +15,37 @@ function klick() {
 
 }
 
-function brown(){
-    {document.getElementById('table').className='brown'};
+
+onload = function() {
+    var body = document.getElementsByTagName("body")[0];
+
+    var tbl = document.createElement("table");
+    tbl.id = "idTable";
+    var tblBody = document.createElement("tbody");
+    for (var j = 0; j <= 7; j++) {
+        var row = document.createElement("tr");
+        for (var i = 0; i <= 7; i++) {
+            var cell = document.createElement("td");
+            cell.innerHTML = 'Paha Privet';
+            row.appendChild(cell);
+            row.appendChild(cell);
+        }
+        tblBody.appendChild(row);
     }
+    tbl.appendChild(tblBody);
+    body.appendChild(tbl);
+
+
+
+
+}
+
+function brown(){
+    {document.getElementById('idTable').className='brown'};
+}
 
 function black(){
-    {document.getElementById('table').className='black'};
+    {document.getElementById('idTable').className='black'};
 }
+
 
